c366ef933dfa364853b280af96c0c5475158c026
TaraJura <63288402+TaraJura@users.noreply.github.com>
2021-01-31 13:42:54 +0100

    hotfix Update 1.3

--- Script.js
+++ Script.js
@@ -100,7 +100,7 @@ var count = document.getElementById("count");
 function Active1() {
 
     document.getElementById("count").innerHTML = +count + 1;
-    count = +count + 100000;
+    count = +count + 1;
 }
 
 
