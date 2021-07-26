702630fad11e0eb838d4602f031e4c9f7170383b
Jiří Novák <63288402+TaraJura@users.noreply.github.com>
2021-07-26 21:42:51 +0200

    BugFix

--- Script.js
+++ Script.js
@@ -1,7 +1,3 @@
-const firebase = require("firebase");
-// Required for side-effects
-require("firebase/firestore");
-
 
 function ExitLoader(){
     var myobj = document.getElementById("loaderBackground");
